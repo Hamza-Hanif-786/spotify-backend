1c345db97b7bc2a9ddc7228f820bc679a1815d87
Hamza Hanif <hamzahanif1024@gmail.com>
2024-11-28 18:09:15 +0500

    SongsController -- updated

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,32 +1,75 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Put,
+  Post,
+  Delete,
+  Param,
+  Body,
+  HttpException,
+  HttpStatus,
+  ParseIntPipe,
+  Inject,
+  Scope,
+} from '@nestjs/common';
 import { SongsService } from './songs.service';
+import { CreateSongDto } from './dto/create-song-dto';
+import { Connection } from 'src/common/constants/connection';
 
-@Controller('songs')
+@Controller({
+  path: 'songs',
+  scope: Scope.REQUEST,
+})
 export class SongsController {
-  constructor(private readonly songsService: SongsService) {}
+  constructor(
+    private songsService: SongsService,
+    @Inject('CONNECTION')
+    private connection: Connection,
+  ) {
+    console.log(`Connection string: ${this.connection.CONNECTION_STRING}`);
+    console.log(`Database: ${this.connection.DB}`);
+    console.log(`Database Name: ${this.connection.DBNAME}`);
+  }
 
   @Post()
-  create() {
-    return this.songsService.create('Gujjar is back');
+  create(@Body() createSongDTO: CreateSongDto) {
+    return this.songsService.create(createSongDTO);
   }
 
   @Get()
   findAll() {
-    return this.songsService.findAll();
+    try {
+      return this.songsService.findAll();
+    } catch (error) {
+      // console.log('My Custom error message using consolelog', error);
+      throw new HttpException(
+        'Server Error',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+        {
+          cause: error,
+        },
+      );
+    }
   }
 
   @Get(':id')
-  findOne() {
-    return 'This action returns a song by id';
+  findOne(
+    @Param(
+      'id',
+      new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE }),
+    )
+    id: number,
+  ) {
+    return `This action returns a song ${id} and typeof of id is ${typeof id}`;
   }
 
   @Put(':id')
-  update() {
-    return 'This action updates a song by id';
+  update(@Param('id') id: number) {
+    return `This action updates a #${id} song`;
   }
 
   @Delete(':id')
-  remove() {
-    return 'This action removes a song by id';
+  remove(@Param('id') id: number) {
+    return `This action removes a #${id} song`;
   }
 }
