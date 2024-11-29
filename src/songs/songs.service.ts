d98208af149f69c489b949dc9e368020732ac8fe
Hamza Hanif <hamzahanif1024@gmail.com>
2024-11-29 21:18:28 +0500

    SongsServiceUpdated

--- src/songs/songs.service.ts
+++ src/songs/songs.service.ts
@@ -1,18 +1,34 @@
-import { Injectable, Scope } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Song } from './song.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateSongDto } from './dto/create-song-dto';
 
-@Injectable({
-  scope: Scope.DEFAULT,
-})
+@Injectable()
 export class SongsService {
-  private readonly songs = [];
+  constructor(
+    @InjectRepository(Song) private songsRepository: Repository<Song>,
+  ) {}
 
-  create(song) {
-    this.songs.push(song);
-    return this.songs;
+  async create(songDTO: CreateSongDto): Promise<Song> {
+    const song = new Song();
+    song.title = songDTO.title;
+    song.artists = songDTO.artists;
+    song.releasedDate = songDTO.releasedDate;
+    song.duration = songDTO.duration;
+    song.lyrics = songDTO.lyrics;
+    return await this.songsRepository.save(song);
   }
 
-  findAll() {
-    // throw new Error('Error in db while fetching records');
-    return this.songs;
+  findAll(): Promise<Song[]> {
+    return this.songsRepository.find();
+  }
+
+  findOne(id: number): Promise<Song> {
+    return this.songsRepository.findOneBy({ id });
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.songsRepository.delete(id);
   }
 }
