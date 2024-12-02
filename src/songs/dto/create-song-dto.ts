16961bf2e11bc01424b8bc69d692ec2a624dfd0d
Hamza Hanif TeamX <hamzahanif@teamx.global>
2024-12-02 12:15:31 +0500

    CreateSongDto updated

--- src/songs/dto/create-song-dto.ts
+++ src/songs/dto/create-song-dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsArray,
   IsDateString,
@@ -10,22 +11,27 @@ import {
 export class CreateSongDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({ example: 'My Heart Will Go On' })
   title: string;
 
   @IsNotEmpty()
   @IsArray()
   @IsString({ each: true })
+  @ApiProperty({ example: ['Celine Dion', 'Royals'] })
   artists: string[];
 
   @IsNotEmpty()
   @IsDateString()
+  @ApiProperty({ example: '2024-12-31' })
   releasedDate: Date;
 
   @IsMilitaryTime()
   @IsNotEmpty()
+  @ApiProperty({ example: '03:00' })
   duration: Date;
 
   @IsString()
   @IsOptional()
+  @ApiProperty({ example: 'https://www.youtube.com/watch?v=1w7OgIMMRc4' })
   readonly lyrics: string;
 }
